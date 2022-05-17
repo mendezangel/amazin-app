40babcb963f862fb46fcaeed65823db0193373bd
mendezangel <id.angelmendez@gmail.com>
2022-05-17 16:05:50 -0700

    added seed data for reviews

---
+++ app/seeds/reviews.py
@@ -0,0 +1,18 @@
+from app.models import db, User, Review
+from faker import Faker
+
+def seed_reviews():
+  fake = Faker()
+  for x in range(300):
+    review = Review(
+      owner_id=fake.random_int(min=1, max=30),
+      product_id=fake.random_int(min=1, max=100),
+      description=fake.paragraph(nb_sentences=5),
+      rating=fake.random_int(min=1, max=5)
+    )
+    db.session.add(review)
+  db.session.commit()
+
+def undo_reviews():
+  db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+  db.session.commit()
---
+++ react-app/src/components/ReviewsUnderProduct/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ReviewsUnderProduct.css'
+
+export default function ReviewsUnderProduct() {
+  return (
+    <div>
+
+    </div>
+  )
+}
