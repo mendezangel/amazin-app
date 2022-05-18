36832d3f715387587f371172833da025b7e5fb08
mendezangel <id.angelmendez@gmail.com>
2022-05-17 22:10:10 -0700

    can now read product reviews

--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useHistory, useParams } from 'react-router-dom'
+import ReviewsUnderProduct from '../ReviewsUnderProduct'
 import './ProductPage.css'
 import { loadProductReviews } from '../../store/review'
 
@@ -69,70 +70,75 @@ export default function ProductPage() {
   if (!loaded) return null;
 
   return (
-    <div className='product-page-11940'>
+    <>
+      <div className='product-page-11940'>
 
-      <div className='product-details-wrapper'>
-        <div className='product-details-img-container'>
-          <img src={`${product.image_url}`} />
-        </div>
-        <div className='product-details-container'>
-          <div className='product-details-child1'>
-            <h2>{product.name}</h2>
+        <div className='product-details-wrapper'>
+          <div className='product-details-img-container'>
+            <img src={`${product.image_url}`} />
           </div>
-          <div className='product-details-child2'>
-            <div className='product-card-price-container'>
-              <div className="dollar-sign">$</div>
-              <div className="price-dollar">{product.price}</div>
-              <div className="price-cents-container">
-                <p className='price-cents'>99</p>
+          <div className='product-details-container'>
+            <div className='product-details-child1'>
+              <h2>{product.name}</h2>
+            </div>
+            <div className='product-details-child2'>
+              <div className='product-card-price-container'>
+                <div className="dollar-sign">$</div>
+                <div className="price-dollar">{product.price}</div>
+                <div className="price-cents-container">
+                  <p className='price-cents'>99</p>
+                </div>
+                <p className='free-returns-p'>& <span className='free-returns'>FREE Returns</span></p>
               </div>
-              <p className='free-returns-p'>& <span className='free-returns'>FREE Returns</span></p>
+            </div>
+            <div className='product-details-child3'>
+              <h4>About this item</h4>
+              <p>{product.description}</p>
             </div>
           </div>
-          <div className='product-details-child3'>
-            <h4>About this item</h4>
-            <p>{product.description}</p>
+          <div className='product-details-buy-container'>
+            <p className='price21549'>${product.price}.99</p>
+            <p className='free-returns-p'>& <span className='free-returns'>FREE Returns</span></p>
+            <p className='free-delivery-p'><span className='FREE-delivery'>FREE delivery</span> {freeDelivery}</p>
+            <p className='fastest-delivery-p'>Or fastest delivery <span className='fastest-delivery'>{fastDelivery}. </span>Order within <span className='order-within-text'>{orderWithin}</span></p>
+            {/* <p className='order-within-p'></p> */}
+            {inStock && (<h3 className='in-stock-h3'>In Stock.</h3>)}
+            {!inStock && (<h3 className='out-of-stock-h3'>Out of Stock.</h3>)}
+            <select className='qty-select' onChange={updateQuantity} disabled={selectDisabled}>
+              <option value='1'>Qty: 1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6</option>
+              <option value='7'>7</option>
+              <option value='8'>8</option>
+              <option value='9'>9</option>
+            </select>
+            <div onClick={addToCart} className='button-container15631'>
+              Add to Cart
+            </div>
+            <p className='secure-transaction-p'>
+              <span><i className="fa-solid fa-lock"></i></span>
+              Secure transaction
+            </p>
+            <div className='ships-from-container'>
+              <div className='ships-from-text-container'>Ships from</div>
+              <div className='website-text-container'>amazin.com</div>
+            </div>
+            <div className='sold-by-container'>
+              <div className='sold-by-text'>Sold by</div>
+              <div className='website-text-container'>amazin.com</div>
+            </div>
+            <p className='return-policy-text'>
+              Return policy: <span>{returnable}</span>
+            </p>
           </div>
-        </div>
-        <div className='product-details-buy-container'>
-          <p className='price21549'>${product.price}.99</p>
-          <p className='free-returns-p'>& <span className='free-returns'>FREE Returns</span></p>
-          <p className='free-delivery-p'><span className='FREE-delivery'>FREE delivery</span> {freeDelivery}</p>
-          <p className='fastest-delivery-p'>Or fastest delivery <span className='fastest-delivery'>{fastDelivery}. </span>Order within <span className='order-within-text'>{orderWithin}</span></p>
-          {/* <p className='order-within-p'></p> */}
-          {inStock && (<h3 className='in-stock-h3'>In Stock.</h3>)}
-          {!inStock && (<h3 className='out-of-stock-h3'>Out of Stock.</h3>)}
-          <select className='qty-select' onChange={updateQuantity} disabled={selectDisabled}>
-            <option value='1'>Qty: 1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
-            <option value='6'>6</option>
-            <option value='7'>7</option>
-            <option value='8'>8</option>
-            <option value='9'>9</option>
-          </select>
-          <div onClick={addToCart} className='button-container15631'>
-            Add to Cart
-          </div>
-          <p className='secure-transaction-p'>
-            <span><i className="fa-solid fa-lock"></i></span>
-            Secure transaction
-          </p>
-          <div className='ships-from-container'>
-            <div className='ships-from-text-container'>Ships from</div>
-            <div className='website-text-container'>amazin.com</div>
-          </div>
-          <div className='sold-by-container'>
-            <div className='sold-by-text'>Sold by</div>
-            <div className='website-text-container'>amazin.com</div>
-          </div>
-          <p className='return-policy-text'>
-            Return policy: <span>{returnable}</span>
-          </p>
         </div>
       </div>
-    </div>
+      <div className='reviews-under-product-container'>
+        <ReviewsUnderProduct reviews={reviews} />
+      </div>
+    </>
   )
 }
--- react-app/src/components/ReviewsUnderProduct/index.js
+++ react-app/src/components/ReviewsUnderProduct/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ReviewsUnderProduct.css'
 
 export default function ReviewsUnderProduct({ reviews }) {
+
+  const dateString = (date) => {
+    const arr = date.split(' ');
+    return `${arr[1]} ${arr[2]}, ${arr[3]}`;
+  }
+
   return (
     <div className='product-reviews-section-container'>
       <div className='reviews-section-child1'>
         <h2>Customer reviews</h2>
+        <p className='global-review-count'>{reviews.length} global ratings</p>
         <div className='review-this-product'>
           <h3>Review this product</h3>
           <p>Share your thoughts with other customers</p>
@@ -14,11 +21,14 @@ export default function ReviewsUnderProduct({ reviews }) {
       </div>
       <div className='reviews-section-child2'>
         {reviews.map(review => {
-          <div className='review-container89023'>
-            <p className='reviewee-name'>{review.user.name}</p>
-            <p className='review-creation-date'>{new Date(review.created_at).toDateString()}</p>
-            <p className='review-description2870'>{review.description}</p>
-          </div>
+          return (
+            <div className='review-container89023'>
+              <p className='reviewee-name'>{review.user.name}</p>
+              <p className='review-creation-date'>Reviewed in {review.user.country} {dateString(new Date(review.created_at).toDateString())}</p>
+              <p className='verified-purchase'>Verified Purchase</p>
+              <p className='review-description2870'>{review.description}</p>
+            </div>
+          )
         })}
       </div>
     </div>
