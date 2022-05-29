c9ee5031404084df04cec256bc80737315dee2fc
mendezangel <id.angelmendez@gmail.com>
2022-05-29 13:31:52 -0700

    finished product mapping for order details

--- react-app/src/components/OrderDetails/index.js
+++ react-app/src/components/OrderDetails/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, useParams } from 'react-router-dom';
+import OrderDetailsProduct from './OrderDetailsProduct';
 import './OrderDetails.css'
 
 export default function OrderDetails() {
@@ -9,7 +10,18 @@ export default function OrderDetails() {
   const user = useSelector(state => state.session.user);
 
   const orderPlaced = order.created_at.split(' ');
-  console.log(order)
+
+  const [products, setProducts] = useState([])
+  const [deliveryDate, setDeliveryDate] = useState('')
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    setDeliveryDate(new Date(new Date(order.created_at).setDate(new Date(order.created_at).getDate() + 2)))
+    setProducts(order.products_ordered)
+    setLoaded(true)
+  }, [])
+
+  if (!loaded) return null;
 
   return (
     <div className='order-details-container5239'>
@@ -21,8 +33,8 @@ export default function OrderDetails() {
         <div className='shipping-address-container'>
           <p className='shipping-address'>Shipping Address</p>
           <p className='user-name'>{user.name}</p>
-          <p className='user-street'>{user.address}</p>
-          <p className='user-city-state'>{user.city}, {user.state} {user.zip_code}</p>
+          <p className='user-street'>{user.address.toUpperCase()}</p>
+          <p className='user-city-state'>{user.city.toUpperCase()}, {user.state} {user.zip_code}</p>
           <p className='user-country'>{user.country}</p>
         </div>
         <div className='delivery-instructions-container'>
@@ -34,7 +46,27 @@ export default function OrderDetails() {
           <p className='grand-total-number'>${order.total_cost}</p>
         </div>
       </div>
-      {/* map here */}
+      <div className='details-child3-9729'>
+        <div className='delivery-date-container'>
+          {order.created_at && deliveryDate < new Date() ?
+            <h2 className='order-delivered-text'>Delivered {deliveryDate.toDateString().split(' ')[1]} {deliveryDate.toDateString().split(' ')[2]}, {deliveryDate.toDateString().split(' ')[3]}</h2>
+            :
+            <h2 className='order-not-delivered-text'>Expected Delivery: {deliveryDate.toDateString().split(' ')[1]} {deliveryDate.toDateString().split(' ')[2]}, {deliveryDate.toDateString().split(' ')[3]}</h2>
+          }
+        </div>
+        <div className='products-bts-container'>
+          <div className='products-container'>
+            {products.map(product => {
+              return (
+                <OrderDetailsProduct product={product} key={product.id} />
+              )
+            })}
+          </div>
+          <div className='buttons-container'>
+            Archive order
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
